b3af6b7c2603ed22437da836fb8c21d7cc1ea20a
Kateryna Rieznik <rieznik.k@gmail.com>
2025-04-19 12:42:09 +0300

    get tracks with react-query

--- src/pages/TracksList.tsx
+++ src/pages/TracksList.tsx
@@ -1,8 +1,13 @@
+import { useGetTracks } from "@/queries";
+
 export function TracksList() {
+  const { data } = useGetTracks();
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-teal-500">Tracks List</h1>
       <p>This is the tracks list page.</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
 }
---
+++ src/queries.ts
@@ -0,0 +1,19 @@
+import { useQuery, UseQueryResult } from "@tanstack/react-query";
+
+import { ITrack } from "@/types";
+
+const API_BASE_URL = "http://0.0.0.0:3000/api";
+
+export const useGetTracks = (): UseQueryResult<ITrack> =>
+  useQuery({
+    queryKey: ["tracks"],
+    queryFn: async () => {
+      const response = await fetch(API_BASE_URL + "/tracks");
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json().then((data) => data.data);
+    },
+  });
